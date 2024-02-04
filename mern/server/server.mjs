013951b6aa40f009dd2d95962ce503a1a392e472
dufresne <maxwjs03@gmail.com>
2024-02-04 12:54:33 -0500

    update backend

---
+++ mern/server/server.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import users from "./routes/user.mjs";
+
+const PORT = 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", users);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
