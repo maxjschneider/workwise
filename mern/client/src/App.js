e6ad2fd7b179faacf0eee13fe41e1c949e2e930d
maxschneider <maxschneider@vt.edu>
2024-02-05 16:58:40 -0500

    add logo and create navbar

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom"
 
 // import components here
+import Navbar from './components/navbar';
 import Schedule from './components/schedule';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <div style={{ margin: 20 }}></div>
       <Routes>
         <Route exact path="/" element={<Schedule />} />      </Routes>
     </div>
---
+++ mern/client/src/components/navbar.js
@@ -0,0 +1,37 @@
+import React from "react"
+import logo from "../images/transparent_logo.png"
+
+import { NavLink } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink className="navbar-brand" to="/">
+               <img alt="MongoDB logo" style={{"width" : 25 + '%'}} src={"logo"}></img>
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+     
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/create">
+                    Create Record
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+    );
+}
