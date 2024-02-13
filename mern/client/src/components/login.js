517bc5ce9ed4758d9d41b9ddc6283d0d771dd1f3
isaiahh <isaiahh@vt.edu>
2024-02-13 12:01:32 -0500

    login front end

--- mern/client/src/components/login.js
+++ mern/client/src/components/login.js
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { login } from "../actions/session";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Col';
 
 const mapStateToProps = ({ errors }) => ({
   errors
@@ -23,22 +26,56 @@ const Login = ({ errors, login }) => {
     }  
   
     return (
-        <>
-        <h1>Login</h1>
-        <p>{errors}</p>
-        <form onSubmit={handleSubmit}>
-            <label>
-            Email:
-            <input type="email" name="email" />
-            </label>
-            <label>
-            Password:
-            <input type="password" name="password" />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
-        <Link to="/register">Don't have an account?</Link>
-        </>
+      <>
+      <Stack gap={2} className="col-md-7 mx-auto">
+          <h1>Login</h1>
+
+          <br />
+
+          <Form onSubmit={handleSubmit}>
+
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <p>{errors}</p>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+
+            <br/>
+            <br/>
+            <Link to="/register">Register</Link>
+
+          </Form>
+
+      </Stack>
+    </>
+
+        // <>
+        // <h1>Login</h1>
+        // <p>{errors}</p>
+        // <form onSubmit={handleSubmit}>
+        //     <label>
+        //     Email:
+        //     <input type="email" name="email" />
+        //     </label>
+        //     <label>
+        //     Password:
+        //     <input type="password" name="password" />
+        //     </label>
+        //     <input type="submit" value="Submit" />
+        // </form>
+        // <Link to="/register">Don't have an account?</Link>
+        // </>
+        
     );
 };
 
