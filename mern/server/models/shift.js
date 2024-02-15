6b58378ec8f832981a3e11da8427a2d4173b96a5
Max Schneider <maxschneider@vt.edu>
2024-02-15 12:03:56 -0500

    create shift schema

---
+++ mern/server/models/shift.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const ScheduleEntrySchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date,
+    required: true,
+    default: new Date("1975-11-11T11:11:11.111+00:00")
+  
+  }
+});
+
+const ScheduleEntry = mongoose.model("schedule", ScheduleEntrySchema, "schedule");
+export default ScheduleEntry;
