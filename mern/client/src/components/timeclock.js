4ebd86e89a532678c14fce0c3422bf6bc8d5058f
dhalterman75 <dhalterman75@gmail.com>
2024-02-23 20:09:21 -0500

    added employee hours screen on timeclock

--- mern/client/src/components/timeclock.js
+++ mern/client/src/components/timeclock.js
@@ -8,10 +8,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import Clock from "./utils/clock"
 import { getUser, clockIn, clockOut, getUserStatus } from "../util/user"
 
+const getTime = (dateString) => {
+  var d = new Date(dateString);
+  d.setHours(d.getHours() - 1);
+
+  return d.toLocaleTimeString("en-US", { hour:"numeric", minute:"2-digit", timeZone:"UTC" });
+}
+
 export default function TimeClock() {
   const [message, setMessage] = useState("");
   const [user, setUser] = useState(null);
-  const [status, setStatus] = useState({ hours: 0.0, clockedIn: false, shifts: [], shifts: [] });
+  const [status, setStatus] = useState({ hours: 0.0, clockedIn: false, shifts: [] });
   
   useEffect(() => {
     fetchData();
@@ -24,6 +31,18 @@ export default function TimeClock() {
     setUser(sessionUser);
     setStatus(status);
   }
+
+  const ShiftRow = (props) => (
+    <tr key={props.entry._id}>
+        <td>
+            <h6 style={{fontSize:15}}>
+             <p>  {getTime(props.entry.start)} - {getTime(props.entry.end)} </p>
+            </h6>
+        </td>
+    </tr>
+                
+
+)
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -77,15 +96,35 @@ export default function TimeClock() {
         </Col>
           
         <Col>
-        
-        <>
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Employee Hours</h1>
+       <h2>Total Time: {status.hours.toFixed(2)} hours</h2>
+
+          <br />
+
+        <table className="table table-borderless">
+            <tbody>
+               
         {
-           // total hours goes here
-           
-           // hours for current user are stored in status.hours
-           // list of previous shifts are stored in status.shifts
+            (status.shifts).map((entry, i) => {
+                return (
+                    <ShiftRow 
+                        key={i} 
+                        entry={entry}
+                    />
+                )
+            })
         }
-        </>
+                    </tbody>
+        </table>
+
+
         
         </Col>
       </Row>
